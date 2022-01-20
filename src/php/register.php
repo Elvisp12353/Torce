610492903c6f604ae2ef21391ab961693dc23a7d
Elvis Genao <elvisp12353@gmail.com>
2022-01-19 20:49:58 -0400

    "Save values on Database"

---
+++ src/php/register.php
@@ -0,0 +1,10 @@
+<?php
+#Recoge los datos del formulario
+$name = $_POST["Name"];
+$lastname = $_POST["LastName"];
+$email = $_POST["Email"];
+$password = $_POST["Password"];
+$profile = $_POST["ProfilePic"];
+echo $name
+
+?>
